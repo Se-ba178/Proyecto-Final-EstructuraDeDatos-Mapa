5aae5b4a8feae362d559d311e4b4c008e4cfc7d9
Bryan <bryancollaguazo08@gmail.com>
2026-07-19 02:05:22 -0500

    Practica

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
-import java.awt.BorderLayout;
+import java.awt.*;
+
+
 import javax.swing.*;
 
 public class App {
@@ -12,16 +14,56 @@ public class App {
         frmMiVentana.setLayout(new BorderLayout());
 
         // Barra de herramientas
-        JToolBar barra = new JToolBar("Opciones Para el Mapa ");
+        JMenuBar menu= new JMenuBar();
+        menu.setSize(50, 10);
+        JMenu archivo= new JMenu("Acciones");
+        JMenu salir= new JMenu("\t X");
+        JToolBar barra= new JToolBar();
         barra.setFloatable(true);
+        barra.setOrientation(JToolBar.VERTICAL);
+        barra.setPreferredSize(new Dimension(150, 400));
+        JPanel menuLateral=new JPanel(new BorderLayout());
+        menuLateral.setPreferredSize(new Dimension(50, 100));
+        JPanel menuInferior= new JPanel(new GridLayout());
+        menuInferior.setLayout(new GridLayout(4,1,5,5));
+       
 
-        JButton btnAbrir = new JButton("Abrir");
-        JButton btnGuardar = new JButton("Guardar");
-        JButton btnEliminar = new JButton("Eliminar");
+        //Botones
+        JButton play= new JButton("Ejecutar");
+        JButton limpiar= new JButton("Limpiar");
+        JButton inicio= new JButton("Marcar como inicio");
+        JButton finale= new JButton("Marcar final");
 
-        barra.add(btnAbrir);
-        barra.add(btnGuardar);
-        barra.add(btnEliminar);
+
+
+
+        //JToolBar barra = new JToolBar("Opciones Para el Mapa ");
+        //barra.setFloatable(true);
+        //barra.setSize(150,160);
+        //Parte del menu para guardar elimiar o abrir el archivo
+        JMenuItem Abrir = new JMenuItem("Abrir");
+        JMenuItem guardar = new JMenuItem("Guardar");
+        JMenuItem eliminar = new JMenuItem("Eliminar");
+        JMenuItem salirD= new JMenuItem("Salir");
+
+       
+
+        archivo.add(Abrir);
+        archivo.add(guardar);
+        archivo.add(eliminar);
+        salir.add(salirD);
+        menuInferior.add(play);
+        menuInferior.add(limpiar);
+        menuInferior.add(inicio);
+        menuInferior.add(finale);
+
+        menu.add(archivo);
+        menu.add(salir);
+        menuLateral.add(menu,BorderLayout.NORTH);
+        menuLateral.add(menuInferior,BorderLayout.SOUTH);
+        barra.add(menuLateral,BorderLayout.CENTER);
+        
+        
 
         // Imagen
         ImageIcon imagen = new ImageIcon("imagenes/Imagen Fondo.png");
@@ -35,11 +77,13 @@ public class App {
         JScrollPane scroll = new JScrollPane(txtResultados);
 
         // Agregar componentes
-        frmMiVentana.add(barra, BorderLayout.NORTH);
+        frmMiVentana.add(barra,BorderLayout.WEST);
         frmMiVentana.add(lblImagen, BorderLayout.CENTER);
         frmMiVentana.add(scroll, BorderLayout.SOUTH);
 
         frmMiVentana.setLocationRelativeTo(null);
+
+        salirD.addActionListener(e -> System.exit(0));
         frmMiVentana.setVisible(true);
     }
 }
